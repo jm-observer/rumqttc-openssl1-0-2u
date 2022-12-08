54b7ccf171eab80c9e231e9cc08cf4958584ffa1
36225 <362254883@qq.com>
2022-12-08 23:14:33 +0800

    added patches

--- src/bin/async-v3-openssl.rs
+++ src/bin/async-v3-openssl.rs
@@ -2,6 +2,7 @@ use tokio::{task, time};
 
 use custom_utils::logger::debug;
 use custom_utils::logger::LevelFilter::Trace;
+use native_tls::Certificate;
 use rumqttc::{self, AsyncClient, MqttOptions, QoS, TlsConfiguration, Transport};
 use std::error::Error;
 use std::time::Duration;
@@ -13,16 +14,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut mqttoptions = MqttOptions::new("test-1", "broker-cn.emqx.io", 8883);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
 
+    let mut builder = native_tls::TlsConnector::builder();
     if let Some(ca) = custom_utils::args::arg_value("--ca", "-c") {
         debug!("TlsConfiguration::SimpleNativeCa");
         let ca = read(ca).await?;
         let ca = rustls_pemfile::certs(&mut ca.as_slice())?.remove(0);
-        let transport = Transport::Tls(TlsConfiguration::SimpleNativeCa { ca: ca });
-        mqttoptions.set_transport(transport);
-    } else {
-        debug!("TlsConfiguration::Native");
-        mqttoptions.set_transport(Transport::Tls(TlsConfiguration::Native));
+        builder.add_root_certificate(Certificate::from_der(ca.as_slice())?);
     }
+    let transport = Transport::Tls(TlsConfiguration::NativeTls(builder));
+    mqttoptions.set_transport(transport);
 
     let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
     task::spawn(async move {
--- src/bin/async-v5-openssl.rs
+++ src/bin/async-v5-openssl.rs
@@ -2,6 +2,7 @@ use rumqttc::v5::mqttbytes::QoS;
 use tokio::{task, time};
 
 use custom_utils::logger::debug;
+use native_tls::Certificate;
 use rumqttc::v5::{AsyncClient, MqttOptions};
 use rumqttc::{TlsConfiguration, Transport};
 use std::error::Error;
@@ -14,16 +15,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut mqttoptions = MqttOptions::new("test-1", "broker-cn.emqx.io", 8883);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
 
+    let mut builder = native_tls::TlsConnector::builder();
     if let Some(ca) = custom_utils::args::arg_value("--ca", "-c") {
         debug!("TlsConfiguration::SimpleNativeCa");
         let ca = read(ca).await?;
         let ca = rustls_pemfile::certs(&mut ca.as_slice())?.remove(0);
-        let transport = Transport::Tls(TlsConfiguration::SimpleNativeCa { ca: ca });
-        mqttoptions.set_transport(transport);
-    } else {
-        debug!("TlsConfiguration::Native");
-        mqttoptions.set_transport(Transport::Tls(TlsConfiguration::Native));
+        builder.add_root_certificate(Certificate::from_der(ca.as_slice())?);
     }
+    let transport = Transport::Tls(TlsConfiguration::NativeTls(builder));
+    mqttoptions.set_transport(transport);
 
     let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
     task::spawn(async move {
@@ -35,8 +35,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let event = eventloop.poll().await;
         println!("{:?}", event?);
     }
-
-    Ok(())
 }
 
 async fn requests(client: AsyncClient) {
